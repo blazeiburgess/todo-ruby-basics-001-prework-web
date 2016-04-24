aecf35aecddcd030d422a8d7a77af894d9d06e24
thedailyephemera <thedailyephemera@gmail.com>
2016-04-24 08:49:51 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+	return num1/num2 
 end
 
 def assign_variable(value)
-  
+  name = value
+	return name
 end
 
-def argue
-  
+def argue(phrase)
+  return phrase
 end
 
-def greeting
-
-end
-
-def return_a_value
+def greeting(greet, name)
 
 end
 
-def last_evaluated_value
-
+def return_a_value(value="Nice")
+	return value
 end
 
-def pizza_party
+def last_evaluated_value(value="expert")
+	return value
+end
 
+def pizza_party(value="cheese")
+	return value
 end
